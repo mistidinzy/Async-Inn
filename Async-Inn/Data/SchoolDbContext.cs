b3429f48546a0d69a59c2e3d64fee3e5ecbc2b4d
mistidinzy <mistidinzy@gmail.com>
2021-10-19 17:06:04 -0500

    setup

---
+++ Async-Inn/Data/SchoolDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
