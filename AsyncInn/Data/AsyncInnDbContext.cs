583c88370e0b311c5abc275aebff063040737b89
mistidinzy <mistidinzy@gmail.com>
2021-10-20 21:22:27 -0500

    create hotel model and controller file

---
+++ AsyncInn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public AsyncInnDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ AsyncInn/Startup.cs
@@ -0,0 +1,57 @@
+﻿using Async_Inn.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AsyncInn
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AsyncInnDbContext>(options =>
+           {
+               string connectionString = Configuration.GetConnectionString("DefaultConnection");
+               options.UseSqlServer(connectionString);
+           });
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+
+
+            });
+        }
+    }
+}
