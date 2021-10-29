f915728d5c5b34dd669d477b558b4d767b715e19
mistidinzy <mistidinzy@gmail.com>
2021-10-29 08:38:27 -0500

    IAmenity repository and databaseAmenityRepository

---
+++ AsyncInn/Services/Database/DatabaseAmenityRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Async_Inn.Data;
+using Async_Inn.Models.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace Async_Inn.Services.Database
+{
+    public class DatabaseAmenityRepository : IAmenityRepository
+    {
+        private readonly AsyncInnDbContext _context;
+
+        public DatabaseAmenityRepository(AsyncInnDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<List<AmenityDTO>> GetAll()
+        {
+            return _context.Amenities
+
+                .Select(amenity => new AmenityDTO
+                {
+                    Id = amenity.Id,
+                    Name = amenity.Name
+                })
+
+                .ToListAsync();
+        }
+    }
+}
