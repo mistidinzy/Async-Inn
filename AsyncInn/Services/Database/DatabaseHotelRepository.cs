3109a86c7fc0cf7e2037c9543615f084be17f7d9
Misti <mistidinzy@gmail.com>
2023-02-24 16:00:36 -0600

    updates readme for lab 13

--- AsyncInn/Services/Database/DatabaseHotelRepository.cs
+++ AsyncInn/Services/Database/DatabaseHotelRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Async_Inn.Data;
 using Async_Inn.Models;
@@ -7,46 +8,52 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Async_Inn.Services.Database
 {
-    public class DatabaseHotelRepository : IHotelRepository
+  public class DatabaseHotelRepository : IHotelRepository
+  {
+    private readonly AsyncInnDbContext _context;
+
+    public DatabaseHotelRepository(AsyncInnDbContext context)
     {
-        private readonly AsyncInnDbContext _context;
-
-        public DatabaseHotelRepository(AsyncInnDbContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<List<Hotel>> GetAll()
-        {
-            return await _context.Hotels.ToListAsync();
-        }
-
-        public async Task<Hotel> GetById(int id)
-        {
-            return await _context.Hotels.FindAsync(id);
-        }
-
-        public async Task Insert(Hotel hotels)
-        {
-            _context.Hotels.Add(hotels);
-
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<bool> TryDelete(int id)
-        {
-            var hotel = await _context.Hotels.FindAsync(id);
-
-            if (hotel == null)
-            {
-                return false;
-            }
-
-            _context.Hotels.Remove(hotel);
-
-            await _context.SaveChangesAsync();
-
-            return true;
-        }
+      _context = context;
     }
+
+    public async Task AddHotel(Hotel hotel)
+    {
+      _context.Hotels.Add(hotel);
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<Hotel>> GetAllHotels()
+    {
+      var hotels = await _context.Hotels.ToListAsync();
+      return hotels;
+    }
+
+    public async Task<Hotel> GetHotelById(int id)
+    {
+      return await _context.Hotels.FindAsync(id);
+    }
+
+    public async Task TryDeleteHotel(int id)
+    {
+      //var hotel = await _context.Hotels.FindAsync(id);
+      //_context.Hotels.Remove(hotel);
+      //await _context.SaveChangesAsync();
+
+      Hotel hotel = await GetHotelById(id);
+      _context.Entry(hotel).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateHotel(int id, Hotel hotel)
+    {
+      _context.Entry(hotel).State = EntityState.Modified;
+      await _context.SaveChangesAsync();
+    }
+
+    private bool HotelExists(int id)
+    {
+      return _context.Hotels.Any(e => e.Id == id);
+    }
+  }
 }
--- README/Lab13.md
+++ README/Lab13.md
@@ -22,14 +22,14 @@ We want to keep the current behavior of our API server the same, only refactorin
 >
 > 2. Build an interface for each of the controllers, that contains the required method signatures to all for CRUD operations to the database directly.
 >
-> 3. Update each of the controllers to inject the interface, rather than the DBContext.
+> 3. Update each of the controllers to inject the ***interface***, rather than the `DBContext`.
 >
-> 4. Create a service for each of the controllers, that implements the appropriate interface.
+> 4. Create a ***service*** for each of the controllers, that implements the appropriate interface.
 >
 > 5. Build out the logic to satisfy the interface, by making the appropriate calls to the db for each action.
 >
-> 6. Update each Controller to use the appropriate method from the interface, rather than the DBContext directly.
+> 6. Update each `Controller` to use the appropriate method from the ***interface***, rather than the `DBContext` directly.
 >
-> 7. Confirm in **POSTMAN** that your controllers are returning the same logic as they did in Lab 12.
+> 7. Confirm in **`POSTMAN`** that your controllers are returning the same logic as they did in Lab 12.
 
 ---
